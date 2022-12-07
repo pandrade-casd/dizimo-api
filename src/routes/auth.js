6118cdc0e08749eed5790dfa8959b53bf878d98e
pandrade-casd <pha_andrade@protonmail.com>
2022-12-06 22:24:33 -0300

    get jwt secret from env vars

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -23,7 +23,7 @@ router.post("/", (req, res, next) => {
 
       jwt.sign(
         { id: user.id },
-        process.env.JWT_SECRET
+        process.env.JWT_SECRET,
         //config.get("jwtsecret"),
         { expiresIn: 43200 },
         (err, token) => {
